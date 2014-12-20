271dead54b9882a57e279b140b34f811c9a86c1e
Azat Mardan <hi@azat.co>
2014-12-20 15:38:42 -0800

    new file

---
+++ api/index-api-11.js
@@ -0,0 +1,114 @@
+var express = require('express')
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+var logger = require('morgan')
+var errorHandler = require('errorhandler')
+var ok = require('okay');
+
+var app = express()
+var dbUri = process.env.MONGOHQ_URL || 'mongodb://localhost:27017/api'
+var dbConnection = mongoose.createConnection(dbUri)
+var Schema = mongoose.Schema
+
+var enumRoles = ['user', 'admin', 'staff']
+
+var postSchema = new Schema ({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    match: /^([\w ,.!?]{1,100})$/,
+    set: function(value) {
+      return value.toUpperCase()
+    },
+    get: function(value) {
+      return value.toLowerCase()
+    }
+  },
+  text: {
+    type: String,
+    required: true,
+    max: 2000
+  },
+  followers: [Schema.Types.ObjectId],
+  meta: Schema.Types.Mixed,
+  comments: [{
+    text: {
+      type: String,
+      trim: true,
+      max: 2000,
+    },
+    author: {
+      id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+      },
+      name: String,
+      role: {
+        type: String,
+        enum: enumRoles
+      }
+    }
+  }],
+  viewCounter: {
+    type: Number
+  },
+  published: Boolean,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+    required: true
+  }
+})
+
+
+var Post = dbConnection.model('Post', postSchema, 'posts')
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', function(req, res){
+  res.send('ok')
+})
+
+app.get('/posts', function(req, res, next){
+  Post.find({}, {limit: 10, sort: {_id: -1}}, ok(next, function(posts){
+    res.send(posts)
+  })
+  )
+})
+
+app.post('/posts', function(req, res, next){
+  var post = new Post (req.body)
+  post.validate(ok(next, function(error){
+    post.save(ok(next, function(results){
+      res.send(results)
+    })
+  )})
+  )
+})
+
+app.get('/posts/:id', function(req, res, next){
+  Post.findOne({_id: req.params.id}, ok(next, function(post){
+    res.send(post.toJSON({getters: true}))
+  }))
+})
+
+app.put('/posts/:id', function(req, res, next){
+  Post.findOne({_id: req.params.id}, ok(next, function(post){
+    post.set(req.body)
+    post.save(ok(next, function(post){
+      res.send(post.toJSON({getters: true}))
+    }))
+  }))
+})
+
+
+app.use(errorHandler())
+
+var server = require('http').createServer(app).listen(3000)
